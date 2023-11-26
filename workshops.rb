3c0e07fdcedb6b35aa7d0b04108c8c820ae8655e
ClauRivera <claudiariveradev@gmail.com>
2023-11-26 11:05:33 -0300

    página de registro de talleres

---
+++ workshops.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+
+def workshop_content(name)
+    File.read("workshops/#{name}.txt")
+rescue Errno::ENOENT
+    return nil
+end
+
+def save_workshop(name, description)
+    File.open("workshops/#{name}.txt", "w") do |file|
+        file.print(description)
+    end
+end
+# Rutas
+def delete_workshop(name)
+    File.delete("workshops/#{name}.txt")
+end
+
+get '/' do
+    @files = Dir.entries("workshops")
+    erb :home
+end
+
+get '/create' do
+    erb :create
+end
+
+get '/:name' do
+    @name = params[:name]
+    @description = workshop_content(@name)
+    erb :workshop 
+end
+
+get '/:name/edit' do
+    @name = params[:name]
+    @description = workshop_content(@name)
+    erb :edit
+end
+
+post '/create' do
+    save_workshop(params["name"], params["description"])
+    @name = params["name"]
+    @message = "creado"
+    erb :message
+end
+
+delete '/:name' do
+    delete_workshop(params[:name])
+    @name = params[:name]
+    @message = "borrado"
+    erb :message
+end
+
+put '/:name' do
+    save_workshop(params[:name], params["description"])
+    redirect URI.escape("/#{params[:name]}")
+end
